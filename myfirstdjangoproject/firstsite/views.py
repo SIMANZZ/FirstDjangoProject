921a9e3705610e058c464a77f73cace5845a3732
SIMANZZ <toxa.blr@mail.ru>
2023-09-07 12:30:33 +0300

    Version 0.2.0
    It's working good omg

--- myfirstdjangoproject/firstsite/views.py
+++ myfirstdjangoproject/firstsite/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Администратор',
-        'title': 'Это первый пост',
-        'content': 'Содержание первого поста.',
-        'date_posted': '12 мая, 2022'
-    },
-    {
-        'author': 'Пользователь',
-        'title': 'Это второй пост',
-        'content': 'Подробное содержание второго поста.',
-        'date_posted': '13 мая, 2022'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'firstsite/home.html', context)
 
